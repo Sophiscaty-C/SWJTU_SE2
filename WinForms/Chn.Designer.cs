dd0a61ba0fcb1e27d4090eeccdfbc183d78db839
Sophiscaty-C <1499660891@qq.com>
2021-06-13 15:00:31 +0800

    test

---
+++ WinForms/Chn.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace WinForms
+{
+    partial class Chn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "搜索结果";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(119, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(638, 284);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "关键字";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Chn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Chn";
+            this.Text = "Chn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WinForms/Eng.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+
+namespace WinForms
+{
+    public partial class Eng : Form
+    {
+        List<string> str = new List<string>();
+        List<string> ti = new List<string>();
+        List<string> ti2 = new List<string>();
+        string[] f = System.IO.Directory.GetFiles("D:/Web/Web_Homework/Eng", "*.txt");
+        public Eng()
+        {
+            InitializeComponent();
+        }
+        private void search_pre()
+        {
+            string[] files = System.IO.Directory.GetFiles("D:/Web/Web_Homework/Eng_af", "*.txt");
+            string text = "";
+            string a = "";
+            string line;
+            int count = 0;
+            foreach (string file in files)
+            {
+                StreamReader reader = new StreamReader(file);
+                count = 0;
+                text = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    count++;
+                    if (count == 1) a = line;
+                    else text += line;
+                }
+                saveIndex(a, text);
+            }
+        }
+        private void saveIndex(string title, string content)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Web\WinForms\index");
+            Lucene.Net.Store.Directory path = new SimpleFSDirectory(dir);
+            bool isNew = false;
+            if (!IndexReader.IndexExists(path))
+            {
+                isNew = true;
+            }
+            Analyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            IndexWriter iw = new IndexWriter(path, analyzer, isNew, IndexWriter.MaxFieldLength.UNLIMITED);
+            Document document = new Document();
+            document.Add(new Field("title", title, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field("content", content, Field.Store.YES, Field.Index.ANALYZED));
+            iw.AddDocument(document); 
+            iw.Optimize(); 
+            iw.Dispose();
+        }
+        private List<string> search(string keyWord)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Web\WinForms\index");
+            Lucene.Net.Store.Directory path = new SimpleFSDirectory(dir);
+            List<string> results = new List<string>();
+            Analyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            IndexSearcher searcher = new IndexSearcher(path);
+            MultiFieldQueryParser parser = new MultiFieldQueryParser(Lucene.Net.Util.Version.LUCENE_30, 
+                new string[] { "title", "content" }, analyzer);
+            Query query = parser.Parse(keyWord);
+            TopDocs td = searcher.Search(query, 50);
+            foreach (ScoreDoc sd in td.ScoreDocs)
+            {
+                Document document = searcher.Doc(sd.Doc);
+                string s = document.Get("title");
+                int x = ti.FindIndex(x=>x.Equals(s));
+                if(!results.Contains(f[x])) results.Add(f[x]);
+            }
+            searcher.Dispose();
+            return results;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            string[] files = System.IO.Directory.GetFiles("D:/Web/Web_Homework/Eng_af", "*.txt");
+            foreach (var s in files)
+            {
+                StreamReader reader = new StreamReader(s);
+                ti.Add(reader.ReadLine());
+            }
+            //search_pre();
+            str =search(key);
+            foreach(var s in str)
+            {
+                StreamReader reader = new StreamReader(s);
+                ti2.Add(reader.ReadLine());
+            }
+            listBox1.DataSource = ti2; 
+        }
+    }
+}
